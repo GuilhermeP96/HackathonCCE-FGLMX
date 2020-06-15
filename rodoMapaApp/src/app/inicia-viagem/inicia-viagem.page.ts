c092b57b95e612c9b816b06511103d337dd43565
marcosricardo <mricardoalves99@gmail.com>
2020-06-14 21:30:02 -0300

    update

--- rodoMapaApp/src/app/inicia-viagem/inicia-viagem.page.ts
+++ rodoMapaApp/src/app/inicia-viagem/inicia-viagem.page.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
+
 
 @Component({
   selector: 'app-inicia-viagem',
@@ -7,9 +18,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IniciaViagemPage implements OnInit {
 
+  map: GoogleMap;
+  public scooters: any = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.loadMap(-23.5342385,-46.895581);
+  }
+
+  loadMap(latitude, longitude) {
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDCa9ZS5MSp3z4zRCS59pQuT9XTu-zB3gw',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDCa9ZS5MSp3z4zRCS59pQuT9XTu-zB3gw'
+    });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: latitude,
+          lng: longitude
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: latitude,
+        lng: longitude
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
   }
 
 }
